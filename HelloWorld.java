0fad249974b45eb1e3811dfe71d2e83179c1ac12
mynameszhazh <1594776259@qq.com>
2023-05-08 00:02:14 +0800

    ci:一个猜数字小游戏

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,23 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("helloworld");
+    Random r = new Random();
+    int number = r.nextInt(100) + 1;
+
+    Scanner sr = new Scanner(System.in);
+
+    while (true) {
+      int num = sr.nextInt();
+      if (num > number) {
+        System.out.println("big le");
+      } else if (num < number) {
+        System.out.println("small");
+      } else {
+        System.out.println("right");
+        break;
+      }
+    }
   }
 }
