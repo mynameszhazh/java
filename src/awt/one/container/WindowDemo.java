0cdda6590ae22b6bf0396c8a7e74b42f9dd5366c
mynameszhazh <1594776259@qq.com>
2023-05-28 11:49:36 +0800

    ci: awt的基本使用

---
+++ src/awt/one/container/WindowDemo.java
@@ -0,0 +1,13 @@
+package src.awt.one.container;
+
+import java.awt.*;
+
+public class WindowDemo {
+    public static void main(String[] args) {
+        Frame fr = new Frame();
+        fr.setLocation(100, 200);
+        fr.setSize(300, 400);
+
+        fr.setVisible(true);
+    }
+}
